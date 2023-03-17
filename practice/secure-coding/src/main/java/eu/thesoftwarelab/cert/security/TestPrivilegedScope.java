8dfbe10f9c8a59ad2bf1a00f2eaedba363d3c0df
mgeorgopoulos <georgopoulosek@dias.com.gr>
2023-03-17 09:24:28 +0200

    Test privileged scope

--- practice/secure-coding/src/main/java/eu/thesoftwarelab/cert/security/TestPrivilegedScope.java
+++ practice/secure-coding/src/main/java/eu/thesoftwarelab/cert/security/TestPrivilegedScope.java
@@ -11,14 +11,14 @@ import java.security.PrivilegedAction;
 public class TestPrivilegedScope {
 
   public static void main(String[] args) {
-    AccessController.doPrivileged((PrivilegedAction) () -> {
+    AccessController.doPrivileged((PrivilegedAction<String>) () -> {
       // privileged scope to write to a file without Security Manager checking permissions for the stack frame.
       try (FileWriter writer = new FileWriter("test.txt")) {
         writer.write("message");
       } catch (IOException e) {
         e.printStackTrace();
       }
-      return null;
+      return "OK";
     });
   }
 
