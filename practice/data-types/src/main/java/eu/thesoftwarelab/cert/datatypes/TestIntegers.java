5e908ce847bca24ceb8d4aabc347cb77907175fb
Emmanouil Georgopoulos <manos.georgopoulos@gmail.com>
2023-06-22 08:34:25 +0300

    Test Integers

--- practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestIntegers.java
+++ practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestIntegers.java
@@ -9,6 +9,9 @@ class TestIntegers {
         Integer i1 = new Integer(1_000_000);
         int i2 = 1_000_000;
         System.out.println(i1 == i2);
+
+        Double d1 = 1_000_000.0;
+        System.out.println(d1 == i2);
     }
 
 }
