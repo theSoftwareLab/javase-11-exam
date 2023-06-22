4ed241edfb087b04130d43d0f3510ada55bf5ec4
Emmanouil Georgopoulos <manos.georgopoulos@gmail.com>
2023-06-22 09:21:06 +0300

    Test Integers

--- practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestIntegers.java
+++ practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestIntegers.java
@@ -6,6 +6,11 @@ package eu.thesoftwarelab.cert.datatypes;
 class TestIntegers {
 
     public static void main(String[] args) {
+
+        int i = 1;
+        int j = i++;
+        System.out.println(j);
+
         Integer i1 = new Integer(1_000_000);
         int i2 = 1_000_000;
         System.out.println(i1 == i2);
