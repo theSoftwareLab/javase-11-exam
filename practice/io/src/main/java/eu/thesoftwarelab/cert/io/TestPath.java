d124eb8e90bd66d9531aac852135232577db620a
Emmanouil Georgopoulos <manos.georgopoulos@gmail.com>
2023-08-25 13:34:52 +0300

    test Path relativize

--- practice/io/src/main/java/eu/thesoftwarelab/cert/io/TestPath.java
+++ practice/io/src/main/java/eu/thesoftwarelab/cert/io/TestPath.java
@@ -9,12 +9,24 @@ import java.nio.file.Paths;
 class TestPath {
 
   public static void main(String[] args) {
+    testNames();
+    testRelativize();
+  }
+
+  private static void testNames() {
     Path path = Paths.get("c:\\java\\TestPath.java");
     System.out.println(path.getRoot());
     System.out.println(path.getNameCount());
     System.out.println(path.getName(0));
     System.out.println(path.getName(1));
     // System.out.println(path.getName(2)); // IllegalArgumentException
+  }
+
+  private static void testRelativize() {
+    Path p1 = Paths.get("x\\y");
+    Path p2 = Paths.get("z");
+    Path p3 = p1.relativize(p2);
+    System.out.println(p3);
   }
 
 }
